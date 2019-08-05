d61b7e14b7705d44cdafd34840c4a66b4a812e7c
Yeremac <yeremac@MIQOs-iMac.local>
2019-08-05 12:27:12 +0400

    add refresh for blog page

--- packages/venia-concept/src/RootComponents/Blog/Blog.js
+++ packages/venia-concept/src/RootComponents/Blog/Blog.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import { RestApi  } from '@magento/peregrine';
-const { request } = RestApi.Magento2;
 
+import Blog_Component from 'src/components/./Blog_Component/./Blog_Component'
 class Blog extends React.Component {
-    getBlogData = () => {
-        const blogUrl = '/wp-json/wp/v2/posts';
-        const response =  request(blogUrl, {
-            method: 'GET'
-        }).then(function(response){
-            return JSON.parse(response.data);
-        });
-        return response;
-    }
+   
     render() {
-        const posts = this.getBlogData();
-        console.log(posts);
+   
         return (
             <div>
-                <h2>This is Bliog Root Component</h2>
-
+                <Blog_Component />
+                {/* {
+                    posts.map(post => 
+                        <p>{post.id}</p>
+                        )
+                } */}
             </div>
         );
     }
 
 }
+
 
 export default Blog;
---
+++ packages/venia-concept/src/components/Blog_Component/action.js
@@ -0,0 +1,7 @@
+// import { createActions } from 'redux-actions';
+// const actionMap = {
+
+// }
+
+
+// export default createActions(actionMap, ...actionTypes, { prefix });
